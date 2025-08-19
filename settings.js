6fd69eb9a5373ab2d3316e55aab89873ffae3e30
pathumboy247-cloud <pathumboy247@gmail.com>
2025-08-19 11:16:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,6 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? ',
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=QB43XYzK#CPzzo5wpfKSq2HPJfRqw1t2nf47XHpN6HsiYQs2_pOc,
+SESSION_NAME: process.env.PORT === undefined ? "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=8Fh13Y7b#jsm3rel7nMQfZxr2wK5dsqsYO80af9Z2OEdXKTQuGbc,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
 };
