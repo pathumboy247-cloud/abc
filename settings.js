a91dd75e4343e207576e33002781575a7e663c28
pathumboy247-cloud <pathumboy247@gmail.com>
2025-10-18 20:52:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? ',
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+PORT: process.env.PORT === undefined ? "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=18150359601275bf" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=VJwBUQSC#5o7k4P7azdtjb0bO69vw15OXHiQ2XJwc4HsH34iShd8,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
 };
